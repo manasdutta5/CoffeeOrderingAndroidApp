9fa0394ce4b3dbd965a4640c684381985f34b66e
Manas Dutta <manas.dutta197@gmail.com>
2021-04-19 18:10:12 +0530

    Added multi language support (Kannada, Hindi and English)

--- app/src/main/java/com/example/coffeeorderingapp/MainActivity.java
+++ app/src/main/java/com/example/coffeeorderingapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.coffeeorderingapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
@@ -19,7 +21,7 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
     boolean whippedCream = false;
-    boolean chocolateTopping=false;
+    boolean chocolateTopping = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,37 +31,50 @@ public class MainActivity extends AppCompatActivity {
 
 
     //    Checkbox whipped cream
-    public void checkWhippedCream(View view){
-        if(!whippedCream) whippedCream = true;
+    public void checkWhippedCream(View view) {
+        if (!whippedCream) whippedCream = true;
         else whippedCream = false;
-        displayPrice(calculatePrice(quantity,10));
-        Log.v("Mainactivity","Yes, function called");
+        displayPrice(calculatePrice(quantity, 10));
+        Log.v("Mainactivity", "Yes, function called");
     }
+
     // check chocolate topping
-    public void checkChocolate(View view){
-        if(!chocolateTopping) chocolateTopping = true;
+    public void checkChocolate(View view) {
+        if (!chocolateTopping) chocolateTopping = true;
         else chocolateTopping = false;
-        displayPrice(calculatePrice(quantity,10));
+        displayPrice(calculatePrice(quantity, 10));
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(createOrderSummary(quantity));
+        EditText name = (EditText) findViewById(R.id.customer_name);
+        String CustomerName = name.getText().toString();
+
+        String message = createOrderSummary(quantity);
+//        String[] to_mail=new String[]{"madu18cs@cmrit.ac.in"};
+//        Intent intent = new Intent(Intent.ACTION_SENDTO);
+//        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+//        intent.putExtra(Intent.EXTRA_EMAIL, to_mail);
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order Details For " + CustomerName);
+//        intent.putExtra(Intent.EXTRA_TEXT, message);
+////        intent.setType("text/plain");
+//        startActivity(Intent.createChooser(intent, "Choose an email app:"));
+            displayMessage(message);
     }
 
     public void incrementOrder(View view) {
         quantity += 1;
         displayQuantity(quantity);
-        displayPrice(calculatePrice(quantity,10));
+        displayPrice(calculatePrice(quantity, 10));
     }
 
     public void decrementOrder(View view) {
-        if(quantity>=1)
+        if (quantity >= 1)
             quantity -= 1;
         displayQuantity(quantity);
-        displayPrice(calculatePrice(quantity,10));
+        displayPrice(calculatePrice(quantity, 10));
     }
 
     /**
@@ -87,34 +102,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     *  This method is used to calculate Price @10 per cup
+     * This method is used to calculate Price @10 per cup
      */
 
-    private int calculatePrice(int n,int price) {
-        if(whippedCream==true)
-        {
-            price+=5;
+    private int calculatePrice(int n, int price) {
+        if (whippedCream == true) {
+            price += 5;
         }
-        if(chocolateTopping)
-        {
-            price+=5;
+        if (chocolateTopping) {
+            price += 5;
         }
 
-        return n*price;
+        return n * price;
     }
 
-    private String createOrderSummary(int quantity)
-    {
-        EditText name=(EditText) findViewById(R.id.customer_name);
+    private String createOrderSummary(int quantity) {
+        EditText name = (EditText) findViewById(R.id.customer_name);
         String CustomerName = name.getText().toString();
-        String msg="Name: "+CustomerName +"\nQuantity: " + quantity +"\nToppings:\n";
-        if(whippedCream) msg+="\t\t\t\tWhipped Cream: Yes\n";
-        else msg+="\t\t\t\tWhipped Cream: No\n";
+        String msg = getString(R.string.Name)+" : "+ CustomerName + "\n"+ getString(R.string.Quantity) + quantity+" : " + "\n"+getString(R.string.Toppings) +"\n";
+        if (whippedCream) msg += "\t\t\t\t"+getString(R.string.whipped_cream)+ " : "+getString(R.string.yes)+"\n";
+        else msg += "\t\t\t\t"+getString(R.string.whipped_cream)+ " : "+" No\n";
 
-        if(chocolateTopping) msg+="\t\t\t\tChocolate: Yes\n";
-        else msg+="\t\t\t\tChocolate: No\n";
+        if (chocolateTopping) msg += "\t\t\t\t"+getString(R.string.chocolate)+ " : " +getString(R.string.yes)+"\n";
+        else msg += "\t\t\t\t"+getString(R.string.chocolate) +getString(R.string.no)+ " : "+"\n";
 
-        msg+="\nPrice: ₹" +calculatePrice(quantity,10) + "\nThank You.";
+        msg += "\n"+getString(R.string.price)+": ₹" + calculatePrice(quantity, 10) + "\n"+getString(R.string.thankyou);
 
         return msg;
     }
